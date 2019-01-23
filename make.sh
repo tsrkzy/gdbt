295e2bc294eb97efeb90b6672373a597cf3e0002
tsrm <tsrmix@gmail.com>
2019-01-23 21:36:56 +0900

    update: compile script

--- make.sh
+++ make.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env
 
 cd `dirname $0`
+SRC_PATH=`pwd`
 DIST_DIR_NAME="dist"
 OS_LIST=("windows" "darwin" "linux")
 ARCH_LIST=("386" "amd64")
 
 set -eu
 
+rm -rf ${DIST_DIR_NAME}
 for OS in ${OS_LIST[@]}
 do
     GOOS=${OS}
@@ -15,8 +17,9 @@ do
         GOARCH=${ARCH}
         GODIST=${DIST_DIR_NAME}/${GOOS}/${GOARCH}
         mkdir -p ${GODIST}
-        echo "compile "${GOOS}"("GOARCH")"
-        go build -o gdbt main.go
-        mv gdbt ${GODIST}/
+        echo "compile "${GOOS}"("${GOARCH}")"
+        cd ${GODIST}
+        env GOOS=${GOOS} GOARCH=${GOARCH} go build -v ${SRC_PATH}/gdbt.go
+        cd ${SRC_PATH}
     done
 done
